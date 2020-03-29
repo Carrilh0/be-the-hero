ac20b7c37393fa10930324ab2d973505e4c12d18
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-03-29 17:38:57 -0300

    retornando ong junto com o incidene

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -9,9 +9,17 @@ module.exports = {
 
         console.log(count);
         const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1 ) * 5)
-        .select('*');
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city', 
+            'ongs.uf'
+        ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
