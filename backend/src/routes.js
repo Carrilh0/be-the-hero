4b47e7486a579f16b0065b57bc7c01bf4eb46032
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-03-29 15:00:29 -0300

    create and read into routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/Hello', (request, response) => {
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
 
-    return response.json({
-        evento: 'Semana omnistack',
-        aluno: 'Vitor Carrilho'
+    return response.json(ongs);
+
+})
+
+routes.post('/ongs', async (request, response) => {
+    const {name, email, whatsapp, city, uf} = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     });
+    
+
+    return response.json({id});
+   
 });
 
 module.exports = routes;
